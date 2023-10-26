7038343955f754ed0a3e045b3e43094e1511c3d7
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-10-26 18:29:14 -0300

    atualização_da_manhã

--- aula01-mp4/app/pages/file-upload/src/app.js
+++ aula01-mp4/app/pages/file-upload/src/app.js
@@ -1,23 +1,36 @@
 import Clock from './deps/clock.js';
-import View from './views';
+import View from './view.js';
 const view = new View();
-
-
-
-btnUploadVideo.addEventListener('click', () => {
-    // trigger file input
-    fileUpload.click()
-})
-let took = ''
-
-function parseBytesIntoMBAndGB(bytes) {
-    const mb = bytes / (1024 * 1024)
-    // if mb is greater than 1024, then convert to GB
-    if (mb > 1024) {
-        // rount to 2 decimal places
-        return `${Math.round(mb / 1024)}GB`
-    }
-    return `${Math.round(mb)}MB`
-}
 const clock = new Clock()
+
+let took = ''
+view.configureOnFileChange(file => {
+    clock.start((time) => {
+        took = time;
+        view.updateElapsedTime(`Process started ${time}`)
+    })
+
+    setTimeout(() => {
+        clock.stop()
+        view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+    }, 5000)
+
+})
+
+
+async function fakeFetch() {
+    const filePath = '/videos/frag_bunny.mp4'
+    const response = await fetch(filePath, {
+        method: "HEAD"
+    })
+    // // traz o tamanho do arquivo:
+    // response.headers.get('content-length')
+    // debugger 
+}
+fakeFetch()
+
+
+
+
+
 
